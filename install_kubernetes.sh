fff9ddafb21ea31899bc78831f032329b590e1bf
Linh B Ngo <linh.b.ngo@gmail.com>
2019-04-06 23:31:33 -0400

    Update install_kubernetes.sh

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -4,9 +4,8 @@ set -x
 
 sudo apt-get update && apt-get install -y apt-transport-https curl
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-sudo deb https://apt.kubernetes.io/ kubernetes-bionic main
-sudo EOF
+sudo touch /etc/apt-sources.list.d/kubernetes.list
+sudo echo  "deb https://apt.kubernetes.io/ kubernetes-bionic main" >> /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
